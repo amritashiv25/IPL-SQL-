a50e0c642c9f3d32642054696e89ccfc50e0b85b
amritashiv25 <shivhareamrita468@gmail.com>
2026-04-28 22:21:18 +0530

    Create Multiplication.java

---
+++ DAY-1-Basics/Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication{
+  public static main void (String[]args){
+    int a = 4;
+    int b = 10;
+    System.out.println("Product = " +(a * b));
+  }
+}
+//output: Product = 40
