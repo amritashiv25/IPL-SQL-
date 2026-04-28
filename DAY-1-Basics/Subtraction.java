fd0c347f42696ac7d2b05733cd71f03c770c50fe
amritashiv25 <shivhareamrita468@gmail.com>
2026-04-28 22:17:43 +0530

    Create Subtraction.java

---
+++ DAY-1-Basics/Subtraction.java
@@ -0,0 +1,7 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 20, b = 5;
+        System.out.println("Difference = " + (a - b));
+    }
+}
+//output: Difference = 15
