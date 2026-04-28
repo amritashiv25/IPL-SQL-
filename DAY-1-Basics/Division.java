2b6be3bf8363f9500ed8d2224886409b744a2519
amritashiv25 <shivhareamrita468@gmail.com>
2026-04-28 22:22:24 +0530

    Create Division.java

---
+++ DAY-1-Basics/Division.java
@@ -0,0 +1,7 @@
+public class Division {
+    public static void main(String[] args) {
+        int a = 20, b = 4;
+        System.out.println("Division = " + (a / b));
+    }
+}
+//output: Division = 5
